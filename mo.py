f58c0dd46fe94ca08d1919f5f40a6b72e2aa91a0
Karako-py <60253379+Karako-py@users.noreply.github.com>
2020-01-24 10:24:20 +0200

    Add files via upload

---
+++ mo.py
@@ -0,0 +1,9 @@
+counter=o
+ap="Ναι"
+while ap=="Ναι" or ap=="ναι":
+    number=int(input("Δώσε βαθμό:))
+    sum+=1
+    couner+=1
+    ap=raw_input("Θες να ξανα δώσεις βαθθμό;:))
+mo=sum/counter
+print "Ο μέσος όρος είναι",mo
